8796fec568cf7a7fa0eb16bc048058602d260b5f
Aaron Mahlke <aaron.mahlke@gmail.com>
2025-09-01 00:09:59 +0200

    update colors again

--- app/components/d-anim/auto-scale/node.vue
+++ app/components/d-anim/auto-scale/node.vue
@@ -8,15 +8,15 @@ const { height } = defineProps<Props>()
 const colorClass = computed(() => {
   if (height > 70) return "bg-orange-500"
   if (height > 30) return "bg-green-500"
-  return "bg-neutral-400"
+  return "bg-neutral-200"
 })
 </script>
 <template>
   <div
-    class="border-neutral bg-surface-strong ease-out-expo flex h-32 w-14 items-end rounded-[18px] border-3 p-0.5 transition-all duration-500"
+    class="border-neutral-subtle bg-surface-subtle ease-out-expo flex h-32 w-14 items-end rounded-[16px] border-3 p-0.5 transition-all duration-500"
   >
     <div
-      class="ease-out-expo w-full rounded-[14px] transition-all duration-300"
+      class="ease-out-expo w-full rounded-[12px] transition-all duration-300"
       :class="colorClass"
       :style="{ height: `${height}%` }"
     />
