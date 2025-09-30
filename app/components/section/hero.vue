95e553dfca21854c468bc2ec159787eb80b4ac2b
Tom Härter <tomuhaerter@gmail.com>
2025-09-30 09:29:27 +0200

    update hero

--- app/components/section/hero.vue
+++ app/components/section/hero.vue
@@ -7,7 +7,7 @@ const { elementId, projectId } = useUnicornStudio("AwvRdOcGsL2B2n5Cake3")
     <d-wrapper>
       <div class="relative z-10 flex max-w-2xl flex-col items-start gap-8">
         <h1 class="text-4xl leading-tight tracking-tight md:text-6xl">
-          The Open Source Cloud That Runs Everything
+          The fastest way to deploy and scale any application
         </h1>
         <p class="text-copy-lg text-neutral">
           <span class="text-neutral">Deploy any application in seconds.</span>
